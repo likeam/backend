4574836987989db9bcb2b916bcf0722d90f182dc
Abdul Rehman <likeam99@gmail.com>
2024-05-27 20:09:56 +0500

    Removing Errors

--- controller/messageController.js
+++ controller/messageController.js
@@ -1,6 +1,7 @@
 import { Message } from "../models/messageSchema.js";
 
 export const sendMessage = async (req, res, next) => {
+    console.log("Rquest Body", req.body);
 
     const { firstName, lastName, email, phone, message } = req.body;
     if(!firstName || !lastName || !email || !phone || !message) {
